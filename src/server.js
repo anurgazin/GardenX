1821f1728a83e09ae02c8246af3624dd296fabef
anurgazin <54969694+anurgazin@users.noreply.github.com>
2022-03-21 22:36:40 -0400

    Article Single Page is Added, Image Structure Changed

--- src/server.js
+++ src/server.js
@@ -8,9 +8,9 @@ const smtpTransport = require("nodemailer-smtp-transport");
 const jwt = require("jsonwebtoken");
 var userScheme = require("./schemes/userScheme");
 var articleScheme = require("./schemes/articleScheme");
-var threadScheme = require('./schemes/threadScheme');
-var commentScheme = require('./schemes/commentsScheme');
-var marketplaceScheme = require('./schemes/marketplaceScheme');
+var threadScheme = require("./schemes/threadScheme");
+var commentScheme = require("./schemes/commentsScheme");
+var marketplaceScheme = require("./schemes/marketplaceScheme");
 const clientSessions = require("client-sessions");
 var multer = require("multer");
 const hbs = require("express-handlebars");
@@ -32,9 +32,7 @@ mongoose.connection.on("open", () => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 let today = new Date().toLocaleDateString("en-US");
-
 
 // Configurations
 const HTTP_PORT = process.env.PORT || 8080;
@@ -59,28 +57,27 @@ function ensureLogin(req, res, next) {
   }
 }
 
-var hbshelper = hbs.create({})
+var hbshelper = hbs.create({});
 
-hbshelper.handlebars.registerHelper('trimString', function(passedString) {
-  var theString = passedString.substring(0,250);
-  return new hbshelper.handlebars.SafeString(theString)
+hbshelper.handlebars.registerHelper("trimString", function (passedString) {
+  var theString = passedString.substring(0, 250);
+  return new hbshelper.handlebars.SafeString(theString);
 });
-hbshelper.handlebars.registerHelper('isAuthor', function(options){
+hbshelper.handlebars.registerHelper("isAuthor", function (options) {
   var hash = options.hash;
   var fnTrue = options.fn;
   var fnFalse = options.inverse;
   return hash.param1 == hash.param2 ? fnTrue(this) : fnFalse(this);
-})
+});
 
-
-const STORAGE = multer.diskStorage({
-  destination: "./public/img/uploadedImg/",
+const ARTICLE_STORAGE = multer.diskStorage({
+  destination: "./public/img/uploadedImg/article",
   filename: function (req, file, cb) {
     console.log("Uploading Photo");
     cb(null, Date.now() + path.extname(file.originalname));
   },
 });
-const UPLOAD = multer({ storage: STORAGE });
+const ARTICLE_UPLOAD = multer({ storage: ARTICLE_STORAGE });
 
 const MARKETPLACE_STORAGE = multer.diskStorage({
   destination: "./public/img/uploadedImg/marketplace",
@@ -114,7 +111,7 @@ app.use(
 );
 // Routes
 app.get("/", function (req, res) {
-  res.redirect("/main")
+  res.redirect("/main");
 });
 // GET Routes for Add Pages
 app.get("/addArticle", ensureLogin, (req, res) => {
@@ -137,7 +134,7 @@ app.get("/addLot", ensureLogin, (req, res) => {
   });
 });
 // GET Routes for Regular Pages
-app.get("/myplants", ensureLogin,(req, res) => {
+app.get("/myplants", ensureLogin, (req, res) => {
   res.render("myplants", {
     user: req.session.user,
     layout: false,
@@ -153,10 +150,10 @@ app.get("/registration", (req, res) => {
     layout: false,
   });
 });
-app.get("/signout", (req,res)=>{
+app.get("/signout", (req, res) => {
   req.session.user = undefined;
   res.redirect("/login");
-})
+});
 app.get("/forgot", (req, res) => {
   res.render("forgot", {
     layout: false,
@@ -198,174 +195,203 @@ app.get("/marketplace", (req, res) => {
         lots: items,
         user: req.session.user,
         layout: false,
-        style: "/css/marketplace.css"
+        style: "/css/marketplace.css",
       });
     });
 });
 // GET Routes for Details Page
 app.get("/threadDetails/:threadId", function (req, res) {
-  const threadId = req.params.threadId
+  const threadId = req.params.threadId;
   threadScheme
     .findById(threadId)
     .lean()
     .exec()
     .then((thread) => {
-      commentScheme.find({'thread': threadId}).lean().exec().then((comments)=>{
-        res.render("threadDetails", {
-          user: req.session.user,
-          details: thread,
-          comment: comments,
-          style: "/css/thread_details.css",
-          layout: false,
+      commentScheme
+        .find({ thread: threadId })
+        .lean()
+        .exec()
+        .then((comments) => {
+          res.render("threadDetails", {
+            user: req.session.user,
+            details: thread,
+            comment: comments,
+            style: "/css/thread_details.css",
+            layout: false,
+          });
         });
-      })
+    });
+});
+
+app.get("/articleDetails/:articleId", function (req, res) {
+  const articleId = req.params.articleId;
+  articleScheme
+    .findById(articleId)
+    .lean()
+    .exec()
+    .then((article) => {
+      res.render("articleDetails", {
+        user: req.session.user,
+        details: article,
+        layout: false,
+      });
     });
 });
 
 app.get("/marketplaceLot/:lotId", async function (req, res) {
-  const lotId = req.params.lotId
+  const lotId = req.params.lotId;
   marketplaceScheme
     .findById(lotId)
     .lean()
     .exec()
     .then((details) => {
       res.render("marketplaceLot", {
-          user: req.session.user,
-          details: details,
-          layout: false,
-          style: "/css/marketplace.css"
+        user: req.session.user,
+        details: details,
+        layout: false,
+        style: "/css/marketplace.css",
       });
     });
 });
 
 // GET and POST Routes for Edit Pages
-app.get("/editArticle/:articleId",ensureLogin, (req,res)=>{
+app.get("/editArticle/:articleId", ensureLogin, (req, res) => {
   const articleId = req.params.articleId;
   articleScheme
     .findById(articleId)
     .lean()
     .exec()
-    .then((article)=>{
-      res.render("editArticle",{
+    .then((article) => {
+      res.render("editArticle", {
         user: req.session.user,
         details: article,
         editMode: true,
         style: "/css/forgot_pass.css",
-        layout: false
+        layout: false,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+app.post(
+  "/editArticle/:articleId",
+  ensureLogin,
+  ARTICLE_UPLOAD.single("photo"),
+  async (req, res) => {
+    const FORM_DATA = req.body;
+    const FORM_FILE = req.file;
+    const articleId = req.params.articleId;
+    if (FORM_FILE) {
+      FORM_FILE.path = FORM_FILE.path.replace("public", "");
+      var article = await articleScheme.findByIdAndUpdate(articleId, {
+        title: FORM_DATA.title,
+        text: FORM_DATA.desc,
+        fileName: FORM_FILE.path,
+      });
+    } else {
+      var article = await articleScheme.findByIdAndUpdate(articleId, {
+        title: FORM_DATA.title,
+        text: FORM_DATA.desc,
+      });
+    }
+    article
+      .save()
+      .then((response) => {
+        console.log(response);
+        res.redirect("/main");
       })
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-})
-app.post("/editArticle/:articleId", ensureLogin, UPLOAD.single("photo"), async (req,res)=>{
-  const FORM_DATA = req.body;
-  const FORM_FILE = req.file;
-  const articleId = req.params.articleId;
-  if(FORM_FILE){
-    FORM_FILE.path = FORM_FILE.path.replace('public','');
-    var article = await articleScheme.findByIdAndUpdate(articleId,{
-      title: FORM_DATA.title,
-      text: FORM_DATA.desc,
-      fileName: FORM_FILE.path
-    })
-  }else{
-    var article = await articleScheme.findByIdAndUpdate(articleId,{
-      title: FORM_DATA.title,
-      text: FORM_DATA.desc,
-    })
+      .catch((err) => {
+        console.log(err);
+      });
   }
-  article
-    .save()
-    .then((response) => {
-      console.log(response);
-      res.redirect("/main");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-})
+);
 
 // Edit Lot(GET and POST requests)
-app.get("/editLot/:lotId",ensureLogin, (req,res)=>{
+app.get("/editLot/:lotId", ensureLogin, (req, res) => {
   const lotId = req.params.lotId;
   marketplaceScheme
     .findById(lotId)
     .lean()
     .exec()
-    .then((lot)=>{
-      res.render("editLot",{
+    .then((lot) => {
+      res.render("editLot", {
         user: req.session.user,
         details: lot,
         editMode: true,
         style: "/css/forgot_pass.css",
-        layout: false
-      })
+        layout: false,
+      });
     })
     .catch((err) => {
       console.log(err);
     });
-})
+});
 
-app.post("/editLot/:lotId", ensureLogin, MARKETPLACE_UPLOAD.single("photo"), async (req,res)=>{
-  const FORM_DATA = req.body;
-  const FORM_FILE = req.file;
-  const lotId = req.params.lotId;
-  if(FORM_FILE){
-    FORM_FILE.path = FORM_FILE.path.replace('public','');
-    var lot = await marketplaceScheme.findByIdAndUpdate(lotId,{
-      title: FORM_DATA.title,
-      description: FORM_DATA.desc,
-      contact: FORM_DATA.email,
-      price: FORM_DATA.price,
-      photo: FORM_FILE.path
-    })
-  }else{
-    var lot = await marketplaceScheme.findByIdAndUpdate(lotId,{
-      title: FORM_DATA.title,
-      description: FORM_DATA.desc,
-      contact: FORM_DATA.email,
-      price: FORM_DATA.price,
-    })
+app.post(
+  "/editLot/:lotId",
+  ensureLogin,
+  MARKETPLACE_UPLOAD.single("photo"),
+  async (req, res) => {
+    const FORM_DATA = req.body;
+    const FORM_FILE = req.file;
+    const lotId = req.params.lotId;
+    if (FORM_FILE) {
+      FORM_FILE.path = FORM_FILE.path.replace("public", "");
+      var lot = await marketplaceScheme.findByIdAndUpdate(lotId, {
+        title: FORM_DATA.title,
+        description: FORM_DATA.desc,
+        contact: FORM_DATA.email,
+        price: FORM_DATA.price,
+        photo: FORM_FILE.path,
+      });
+    } else {
+      var lot = await marketplaceScheme.findByIdAndUpdate(lotId, {
+        title: FORM_DATA.title,
+        description: FORM_DATA.desc,
+        contact: FORM_DATA.email,
+        price: FORM_DATA.price,
+      });
+    }
+    lot
+      .save()
+      .then((response) => {
+        console.log(response);
+        res.redirect("/marketplace");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
-  lot
-    .save()
-    .then((response) => {
-      console.log(response);
-      res.redirect("/marketplace");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-})
+);
 
 // Edit Thread (GET and POST requests)
-app.get("/editThread/:threadId",ensureLogin, (req,res)=>{
+app.get("/editThread/:threadId", ensureLogin, (req, res) => {
   const threadId = req.params.threadId;
   threadScheme
     .findById(threadId)
     .lean()
     .exec()
-    .then((thread)=>{
-      res.render("editThread",{
+    .then((thread) => {
+      res.render("editThread", {
         user: req.session.user,
         details: thread,
         editMode: true,
         style: "/css/forgot_pass.css",
-        layout: false
-      })
+        layout: false,
+      });
     })
     .catch((err) => {
       console.log(err);
     });
-})
-app.post("/editThread/:threadId", ensureLogin, async (req,res)=>{
+});
+app.post("/editThread/:threadId", ensureLogin, async (req, res) => {
   const FORM_DATA = req.body;
   const threadId = req.params.threadId;
-    var thread = await threadScheme.findByIdAndUpdate(threadId,{
-      title: FORM_DATA.title,
-      text: FORM_DATA.desc,
-    })
+  var thread = await threadScheme.findByIdAndUpdate(threadId, {
+    title: FORM_DATA.title,
+    text: FORM_DATA.desc,
+  });
   thread
     .save()
     .then((response) => {
@@ -375,34 +401,34 @@ app.post("/editThread/:threadId", ensureLogin, async (req,res)=>{
     .catch((err) => {
       console.log(err);
     });
-})
+});
 
 // Comment
-app.get("/editComment/:commentId",ensureLogin, (req,res)=>{
+app.get("/editComment/:commentId", ensureLogin, (req, res) => {
   const commentId = req.params.commentId;
   commentsScheme
     .findById(commentId)
     .lean()
     .exec()
-    .then((comment)=>{
-      res.render("editComment",{
+    .then((comment) => {
+      res.render("editComment", {
         user: req.session.user,
         details: comment,
         editMode: true,
         style: "/css/forgot_pass.css",
-        layout: false
-      })
+        layout: false,
+      });
     })
     .catch((err) => {
       console.log(err);
     });
-})
-app.post("/editComment/:commentId", ensureLogin, async (req,res)=>{
+});
+app.post("/editComment/:commentId", ensureLogin, async (req, res) => {
   const FORM_DATA = req.body;
   const commentId = req.params.commentId;
-  var comment = await commentsScheme.findByIdAndUpdate(commentId,{
-      text: FORM_DATA.desc,
-    })
+  var comment = await commentsScheme.findByIdAndUpdate(commentId, {
+    text: FORM_DATA.desc,
+  });
   comment
     .save()
     .then((response) => {
@@ -412,81 +438,85 @@ app.post("/editComment/:commentId", ensureLogin, async (req,res)=>{
     .catch((err) => {
       console.log(err);
     });
-})
+});
 // User
-app.get("/editUser",ensureLogin,(req,res)=>{
+app.get("/editUser", ensureLogin, (req, res) => {
   res.render("editUser", {
     details: req.session.user,
     style: "/css/forgot_pass.css",
-    layout: false
-  })
-})
+    layout: false,
+  });
+});
 
-app.post("/editUser", ensureLogin, async(req,res)=>{
+app.post("/editUser", ensureLogin, async (req, res) => {
   const FORM_DATA = req.body;
   const userId = req.session.user._id;
-  var user = await userScheme.findByIdAndUpdate(userId,{
+  var user = await userScheme.findByIdAndUpdate(userId, {
     firstName: FORM_DATA.firstName,
     lastName: FORM_DATA.lastName,
-    email: FORM_DATA.email
-  })
-  user
-  .save()
-  .then((response)=>{
-    console.log(user.email);
-    req.session.user = undefined;
-    res.redirect("/login")
-  })
-  .catch((err) => {
-    console.log(err);
+    email: FORM_DATA.email,
   });
-})
+  user
+    .save()
+    .then((response) => {
+      console.log(user.email);
+      req.session.user = undefined;
+      res.redirect("/login");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 // GET Requests for Delete pages
-app.get("/deleteArticle/:articleId",ensureLogin,(req,res)=>{
+app.get("/deleteArticle/:articleId", ensureLogin, (req, res) => {
   const articleId = req.params.articleId;
-  articleScheme.findByIdAndDelete(articleId)
-  .then((response)=>{
-    console.log(response);
-    res.redirect("/main");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-})
-app.get("/deleteLot/:lotId",ensureLogin,(req,res)=>{
+  articleScheme
+    .findByIdAndDelete(articleId)
+    .then((response) => {
+      console.log(response);
+      res.redirect("/main");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+app.get("/deleteLot/:lotId", ensureLogin, (req, res) => {
   const lotId = req.params.lotId;
-  marketplaceScheme.findByIdAndDelete(lotId)
-  .then((response)=>{
-    console.log(response);
-    res.redirect("/marketplace");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-})
-app.get("/deleteThread/:threadId",ensureLogin,(req,res)=>{
+  marketplaceScheme
+    .findByIdAndDelete(lotId)
+    .then((response) => {
+      console.log(response);
+      res.redirect("/marketplace");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+app.get("/deleteThread/:threadId", ensureLogin, (req, res) => {
   const threadId = req.params.threadId;
-  threadScheme.findByIdAndDelete(threadId)
-  .then((response)=>{
-    console.log(response);
-    res.redirect("/thread");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-})
-app.get("/deleteComment/:commentId",ensureLogin,(req,res)=>{
+  threadScheme
+    .findByIdAndDelete(threadId)
+    .then((response) => {
+      console.log(response);
+      res.redirect("/thread");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+app.get("/deleteComment/:commentId", ensureLogin, (req, res) => {
   const commentId = req.params.commentId;
-  commentScheme.findByIdAndDelete(commentId)
-  .then((response)=>{
-    console.log(response);
-    res.redirect("/thread");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-})
+  commentScheme
+    .findByIdAndDelete(commentId)
+    .then((response) => {
+      console.log(response);
+      res.redirect("/thread");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 // POST Request for LOGIN
 app.post("/login", async (req, res) => {
@@ -602,53 +632,63 @@ app.post("/reset-pwd/:id/:token", async (req, res) => {
   }
 });
 // POST request for create
-app.post("/createArticle", ensureLogin, UPLOAD.single("photo"), (req, res) => {
-  const FORM_DATA = req.body;
-  var FORM_FILE = req.file;
-  console.log(FORM_FILE.filename);
-  FORM_FILE.path = FORM_FILE.path.replace('public','');
-  var article = new articleScheme({
-    title: FORM_DATA.title,
-    text: FORM_DATA.desc,
-    date: today,
-    fileName: FORM_FILE.path,
-    author: req.session.user.email,
-  });
-  article
-    .save()
-    .then((response) => {
-      console.log(response);
-      res.redirect("/main");
-    })
-    .catch((err) => {
-      console.log(err);
+app.post(
+  "/createArticle",
+  ensureLogin,
+  ARTICLE_UPLOAD.single("photo"),
+  (req, res) => {
+    const FORM_DATA = req.body;
+    var FORM_FILE = req.file;
+    console.log(FORM_FILE.filename);
+    FORM_FILE.path = FORM_FILE.path.replace("public", "");
+    var article = new articleScheme({
+      title: FORM_DATA.title,
+      text: FORM_DATA.desc,
+      date: today,
+      fileName: FORM_FILE.path,
+      author: req.session.user.email,
     });
-});
+    article
+      .save()
+      .then((response) => {
+        console.log(response);
+        res.redirect("/main");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+);
 
-app.post("/createLot", ensureLogin, MARKETPLACE_UPLOAD.single("photo"), (req, res) => {
-  const FORM_DATA = req.body;
-  var FORM_FILE = req.file;
-  console.log(FORM_FILE.filename);
-  FORM_FILE.path = FORM_FILE.path.replace('public','');
-  var lot = new marketplaceScheme({
-    title: FORM_DATA.title,
-    description: FORM_DATA.desc,
-    contact: FORM_DATA.email,
-    date: today,
-    price: FORM_DATA.price,
-    photo: FORM_FILE.path,
-    owner: req.session.user._id,
-  });
-  lot
-    .save()
-    .then((response) => {
-      console.log(response);
-      res.redirect("/marketplace");
-    })
-    .catch((err) => {
-      console.log(err);
+app.post(
+  "/createLot",
+  ensureLogin,
+  MARKETPLACE_UPLOAD.single("photo"),
+  (req, res) => {
+    const FORM_DATA = req.body;
+    var FORM_FILE = req.file;
+    console.log(FORM_FILE.filename);
+    FORM_FILE.path = FORM_FILE.path.replace("public", "");
+    var lot = new marketplaceScheme({
+      title: FORM_DATA.title,
+      description: FORM_DATA.desc,
+      contact: FORM_DATA.email,
+      date: today,
+      price: FORM_DATA.price,
+      photo: FORM_FILE.path,
+      owner: req.session.user._id,
     });
-});
+    lot
+      .save()
+      .then((response) => {
+        console.log(response);
+        res.redirect("/marketplace");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+);
 
 app.post("/createThread", ensureLogin, (req, res) => {
   const FORM_DATA = req.body;
